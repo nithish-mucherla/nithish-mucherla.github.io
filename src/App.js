53871b04428c95f0cd0b187b8690e5fd52f6dd2a
nithish-mucherla <mucherlanithu@gmail.com>
2021-03-13 17:37:01 +0530

    updated bottom nav
    updated experiences

--- src/App.js
+++ src/App.js
@@ -1,67 +1,67 @@
-import React, { useState } from 'react';
-import './App.css';
-import Navbar from './components/navigation/js/navBar';
-import Home from './components/home/js/home';
-import BottomNav from './components/navigation/js/bottomNav';
-import About from './components/about/js/about';
-import Experience from './components/experience/js/experience';
-import Education from './components/education/js/education';
-import Skills from './components/skills/js/skills';
-import { ThemeProvider, createMuiTheme} from '@material-ui/core/styles';
-import {Fab, Zoom, makeStyles, useScrollTrigger } from '@material-ui/core';
-import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
-import ContactContainer from './components/contact/js/contactContainer';
+import React, { useState } from "react";
+import "./App.css";
+import Navbar from "./components/navigation/js/navBar";
+import Home from "./components/home/js/home";
+import About from "./components/about/js/about";
+import Experience from "./components/experience/js/experience";
+import Education from "./components/education/js/education";
+import Skills from "./components/skills/js/skills";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import { Fab, Zoom, makeStyles, useScrollTrigger } from "@material-ui/core";
+import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
+import ContactContainer from "./components/contact/js/contactContainer";
 
 function App(props) {
-
   //This is a Hook that we are using to swith between dark and light themes.
   // Hook being used : useState()
   // Hooks allow us to use state and other react features without defining a class.
-  
-  const [darkMode, setDarkMode] = useState(localStorage.getItem('darkMode') || 'true');
+
+  const [darkMode, setDarkMode] = useState(
+    localStorage.getItem("darkMode") || "true"
+  );
   const theme = createMuiTheme({
     palette: {
-      type: darkMode==='true' ? 'dark' : 'light',
-      primary :{
-        main: "#0586ff"
-      }
+      type: darkMode === "true" ? "dark" : "light",
+      primary: {
+        main: "#0586ff",
+      },
     },
   });
-  const appClass = darkMode==='true' ? "App dark" : "App";
+  const appClass = darkMode === "true" ? "App dark" : "App";
 
   React.useEffect(() => {
-    localStorage.setItem('darkMode', darkMode);
+    localStorage.setItem("darkMode", darkMode);
   }, [darkMode]);
 
   const switchTheme = () => {
-    darkMode === 'true'? setDarkMode('false') : setDarkMode('true');
-  }
+    darkMode === "true" ? setDarkMode("false") : setDarkMode("true");
+  };
 
   const useStyles = makeStyles((theme) => ({
     root: {
-      position: 'fixed',
+      position: "fixed",
       bottom: theme.spacing(3),
       right: theme.spacing(3),
     },
-  })); 
+  }));
 
   function ScrollTop(props) {
-    const { children} = props;
+    const { children } = props;
     const classes = useStyles();
 
     const trigger = useScrollTrigger({
       disableHysteresis: true,
       threshold: 100,
     });
-  
+
     const handleClick = (event) => {
-      const anchor = document.querySelector('#back-to-top-anchor');
-  
+      const anchor = document.querySelector("#back-to-top-anchor");
+
       if (anchor) {
-        anchor.scrollIntoView({ behavior: 'smooth', block: 'center' });
+        anchor.scrollIntoView({ behavior: "smooth", block: "center" });
       }
     };
-  
+
     return (
       <Zoom in={trigger}>
         <div onClick={handleClick} role="presentation" className={classes.root}>
@@ -70,11 +70,11 @@ function App(props) {
       </Zoom>
     );
   }
-  
+
   return (
     <ThemeProvider theme={theme}>
       <div className={appClass}>
-        <Navbar switchTheme = {switchTheme} mode={darkMode}/>
+        <Navbar switchTheme={switchTheme} mode={darkMode} />
         <span id="back-to-top-anchor"></span>
         <Home />
         <About />
@@ -82,7 +82,6 @@ function App(props) {
         <Education />
         <Skills />
         <ContactContainer />
-        <BottomNav />
         <ScrollTop {...props}>
           <Fab color="secondary" size="medium" aria-label="scroll back to top">
             <KeyboardArrowUpIcon />
--- src/components/navigation/js/bottomNav.js
+++ src/components/navigation/js/bottomNav.js
@@ -1,24 +1,44 @@
-import { Grid } from '@material-ui/core';
-import React from 'react';
-import '../css/navigation.css';
+import { Grid, IconButton } from "@material-ui/core";
+import React from "react";
+import "../css/navigation.css";
+import { Facebook, Instagram, LinkedIn, GitHub } from "@material-ui/icons";
 
 function BottomNav(props) {
-    return(
-        <Grid 
-            container 
-            className="content"
-            direction="column"
-        >
-            <Grid item>
-                <footer className="bottomNav">
-                    <a href="https://github.com/nithish-mucherla"  rel='noopener' ><i className="fa fa-github"></i></a>
-                    <a href="https://www.instagram.com/nithish_msn/" rel='noopener' ><i className="fa fa-instagram"></i></a>
-                    <a href="https://www.facebook.com/nithu.mucherla" rel='noopener'><i className="fa fa-facebook-f"></i></a>
-                    <a href="https://www.linkedin.com/in/sai-nithish-mucherla" rel='noopener' ><i className="fa fa-linkedin"></i></a>
-                </footer>
-            </Grid>       
-        </Grid>
-    );
+  return (
+    <Grid container className="bottomNav" justify="center">
+      <Grid item>
+        <IconButton>
+          <a href="https://www.instagram.com/nithish_msn/" rel="noopener">
+            <Instagram style={{ color: "grey" }} />
+          </a>
+        </IconButton>
+      </Grid>
+      <Grid item>
+        <IconButton>
+          <a href="https://www.facebook.com/nithu.mucherla" rel="noopener">
+            <Facebook style={{ color: "grey" }} />
+          </a>
+        </IconButton>
+      </Grid>
+      <Grid item>
+        <IconButton>
+          <a
+            href="https://www.linkedin.com/in/sai-nithish-mucherla"
+            rel="noopener"
+          >
+            <LinkedIn style={{ color: "grey" }} />
+          </a>
+        </IconButton>
+      </Grid>
+      <Grid item>
+        <IconButton>
+          <a href="https://github.com/nithish-mucherla" rel="noopener">
+            <GitHub style={{ color: "grey" }} />
+          </a>
+        </IconButton>
+      </Grid>
+    </Grid>
+  );
 }
 
-export default BottomNav
+export default BottomNav;
